1a7a134b39f6785334827f1ab652c0065f93282d
이혜진 <hy2jinl22@gmail.com>
2022-01-19 00:28:54 +0900

    fix - quiz폴더 & user폴더
    - quiz기능의 폴더 경로추가
    - signup 담임여부 확인가능 -> 학년, 반 보이게 하기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import routes from './routes';
 import Header from './components/Header'
+import './App.scss'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <ChakraProvider>
         <Header/>
         {routes.map(route=>{
           return (
-            <Route key={route.path} path={route.path} component = {route.component}/>
+            <Route key={route.path} path={route.path} exact={route.exact} component = {route.component}/>
           )})}
       </ChakraProvider>
     </div>
