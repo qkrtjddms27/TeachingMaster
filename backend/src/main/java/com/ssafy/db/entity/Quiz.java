da763af3d1a7b22444b7520fb0244a0ae249f995
김현태 <zofht2549@hanmail.net>
2022-01-19 11:16:14 +0900

    feat - Teacher Entity @추가
    Teacher Entity에 Column어노테이션 추가

---
+++ backend/src/main/java/com/ssafy/db/entity/Quiz.java
@@ -0,0 +1,47 @@
+package com.ssafy.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Quiz {
+
+    @Id
+    @GeneratedValue
+    private int quizId;
+
+    @Column(length = 10, nullable = false)
+    private String subject;
+
+    @Column(length = 100)
+    private String quizPhoto;
+
+    @Column(length = 20, nullable = false)
+    private String quizTitle;
+
+    @Column(length = 500, nullable = false)
+    private String quizContents;
+
+    @Column(nullable = false)
+    private int quizAnswer;
+
+    private boolean openStatus;
+
+    private int quizTimeout;
+
+    @Column(nullable = false)
+    private int quizGrade;
+
+    @JoinColumn(name = "teacher_id")
+    Teacher teacher;
+
+    private String teacherId;
+
+    private int folderIndex;
+
+
+}
