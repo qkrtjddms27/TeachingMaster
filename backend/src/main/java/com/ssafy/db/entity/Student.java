e324eea57a5312f3e6efd528283ae2037990d781
김현태 <zofht2549@hanmail.net>
2022-01-19 18:05:15 +0900

    feat - Entity추가
    Conference, ConferenceHistory, ConferenceLog, Room, Student Entity추가

--- backend/src/main/java/com/ssafy/db/entity/Student.java
+++ backend/src/main/java/com/ssafy/db/entity/Student.java
@@ -1,4 +1,52 @@
 package com.ssafy.db.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Student {
+
+//  기본키 설정 및 연관 관계 설정 시작
+    @Id
+    @Column(length = 20)
+    private String studentId;
+//  기본키 설정 및 연관 관계 설정 끝
+
+    @Column(nullable = false, length = 20)
+    private String studentName;
+
+    @Column(length = 20)
+    private String studentPhone;
+
+    @Column(length = 100)
+    private String studentProfile;
+
+    private int studentScore;
+
+    @Column(length = 20)
+    private String studentEmail;
+
+    private int countingStar;
+
+    @Column(nullable = false, length = 20)
+    private String parentsName;
+
+    @Column(nullable = false, length = 20)
+    private String address;
+
+    @Column(nullable = false, length = 10)
+    private String relation;
+
+    @Column(length = 20)
+    private String parentsPhone;
+
+    @ManyToOne
+    @JoinColumn(name = "roomId")
+    private Room room;
+
+
 }
