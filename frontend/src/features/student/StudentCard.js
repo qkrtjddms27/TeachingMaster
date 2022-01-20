bdc6926d00b80afc14634c6fbeca61d99f0640db
이혜진 <hy2jinl22@gmail.com>
2022-01-21 06:49:17 +0900

    fix - student
    - 학생 목록 카드 생성
    - 뒤집으면 약간의 정보 제공
    - 누르면 디테일로 넘어감

--- frontend/src/features/student/StudentCard.js
+++ frontend/src/features/student/StudentCard.js
@@ -1,22 +1,44 @@
-import { Heading,Text,Avatar } from '@chakra-ui/react'
+import { Box,Text } from '@chakra-ui/react'
 import React from 'react'
-import { Card,Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
+import { Card } from 'react-bootstrap'
 const StudentCard = ({student}) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top" src={student.profile} /> */}
-      <Avatar size="xl" name='Dan Abrahmov' src={student.profile} />
-      <Card.Body>
-        <Card.Title>{student.name}</Card.Title>
-        <Card.Text>
-        ⭐{student.star}
-        </Card.Text>
-        <Button variant="warning">
-          <Link to="/"> 학생정보</Link>
-          </Button>
-      </Card.Body>
+    <Card className='CARD'>
+      <Box className='f-cardbox' boxShadow="xl">
+        <img className='image' alt='학생사진' 
+          src={student.profile} />
+        <div className='card-body'>
+          <Card.Title className='card-title'>
+              {student.name}
+          </Card.Title>
+          <Card.Text className='stars'>
+          ⭐&nbsp;{student.star}
+          </Card.Text>
+        </div>
+      </Box>
+    
+      <Box className='b-cardbox' id ={student.id%2===0?"odd":"even"} boxShadow="xl">
+        <div className='img-name-star'>
+          <img className='image' alt='학생사진' src={student.profile} />
+          <div>
+            <div className='card-title'>{student.name}</div>
+            <div className='stars'>⭐&nbsp;{student.star}</div>
+          </div>
+        </div>
+        <div className='contents'>
+          <p>누적 ⭐&nbsp;{student.star*2}</p>
+          <p>메일주소&nbsp; {student.email}</p>
+          <p>연락처&nbsp; {student.phone}</p>
+        </div>
+
+        <ul className='memo'>
+          <Text className='memotitle'>메모</Text>
+            <li>우리반 반장</li>
+            <li>선생님을 잘 따른다</li>
+            <li>지난 기말고사 1등</li>
+        </ul>
+        
+      </Box>
     </Card>
   )
 }
---
+++ frontend/src/features/student/StudentDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StudentDetail = () => {
+  return (
+  <div>
+    <div></div>
+  </div>);
+};
+
+export default StudentDetail;
