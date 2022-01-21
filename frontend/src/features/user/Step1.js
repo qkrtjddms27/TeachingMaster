1cad9e19a2407165120b1d4501a9277a25b117e0
이혜진 <hy2jinl22@gmail.com>
2022-01-21 17:20:01 +0900

    fix - user

--- frontend/src/features/user/Step1.js
+++ frontend/src/features/user/Step1.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Text, Checkbox, Stack, Button } from '@chakra-ui/react';
 import './Signup.scss'
+import AlertDialogModal from '../../components/AlertModal';
 
 
 // 약관동의 페이지
@@ -11,15 +12,20 @@ const Step1 = ({ step, setStep }) => {
 
   const content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
+  // 다음버튼 눌렀는데 비밀번호 다를때 쓸 모달이 open인지 close인지
+  const [isOpen, setIsOpen] = useState(false)
   const onClickHandle = () => {
     if (allChecked) {
       setStep(step + 1)
+    } else {
+      setIsOpen(true)
     }
   }
 
 
   return (
     <div>
+      <AlertDialogModal title="동의가 필요합니다" content="필수 이용약관에 동의하지 않으면 서비스 제한이 있습니다" isOpen={isOpen} setIsOpen={setIsOpen} />
       <Stack className='step1-stack'>
         <Box className='step1-first'>
           <Text>홈페이지 이용약관</Text>
--- frontend/src/features/user/Step4.js
+++ frontend/src/features/user/Step4.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
+import { Stack, Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import './Signup.scss'
 
 
 // 회원가입이 완료되었습니다 페이지 -> 로그인 버튼 있으면 좋을듯?
-const Step4 = () => {
+const Step4 = ({ step,setStep }) => {
   return (
     <div>
-      가입완료
+      <Stack className='step3-stack2'>
+        <div className='step-button'>
+          {/* <Button borderRadius={0} variant="solid" className='prev-button' 
+            bgColor="#B5A18C" colorScheme="#c7baac" width="48%" onClick={() => setStep(step-1)}
+          ><Link to="/" className="step4-link">메인으로</Link></Button> */}
+          <Button borderRadius={0} variant="solid" className='prev-button' 
+            bgColor="#B5A18C" colorScheme="#c7baac" width="48%" onClick={() => setStep(step-1)}
+          >이전</Button>
+          <Button borderRadius={0} variant="solid" className="next-button" 
+            bgColor="#7e5526" colorScheme="#472b0a" width="48%" onClick={() => setStep(step+1)}
+          ><Link to="/login" className='step4-link'>로그인</Link></Button>
+        </div>
+      </Stack>
     </div>
   );
 };
