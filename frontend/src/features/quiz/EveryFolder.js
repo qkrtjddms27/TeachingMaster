9a99118c289553ed97c865fdd97d87df1c383c70
이혜진 <hy2jinl22@gmail.com>
2022-01-25 13:20:54 +0900

    fix - 폴더 요청보내고 응답 받음

--- frontend/src/features/quiz/EveryFolder.js
+++ frontend/src/features/quiz/EveryFolder.js
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { folders } from './quizzes'
 import { Heading, Image } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import React,{ useCallback, useEffect, useState } from 'react'
@@ -12,43 +11,68 @@ import AOS from 'aos'
 import "aos/dist/aos.css"
 
 const Folder = () => {
-  useEffect(()=>{
+  const userId = localStorage.getItem("userId")
+
+  // 폴더에 주는 효과
+  useEffect(() => {
     AOS.init()
   })
-  const user_id = localStorage.getItem("userId")
-  const [Imgurls,setImgurls] = useState([])
-  const [folderss,setFolderss] = useState(folders)
+
+  // user의 폴더 목록 받기 -> 토큰사용은 나중으로 미룸
+  const [myFolders, setMyFolders] = useState([])
+  useEffect (() => {
+    axios({
+      url:`http://localhost:8080/api/v1/quiz/folder/${userId}`,
+      method:"GET",
+      // headers: setToken()
+    })
+    .then(({data}) => {
+      setMyFolders(data)
+      console.log(data)
+      return data
+    })
+    .then (folders => {
+      const nums = Array(50).fill().map((v, i) => i+1)
+      const shuffle = []
+      while (nums.length > 0) {
+        shuffle.push(nums.splice())
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      setMyFolders([])
+    })
+  }, [])
+
+
+  // 폴더에 사용할 이미지 주소 만들기
+  const [imgUrls, setImgUrls] = useState([])
+  const [folderss, setFolderss] = useState(myFolders)
   const getNumbers = ()=>{
     const Nums = Array(50).fill().map((v,i)=>i+1)
     const shuffle = []
     while (Nums.length>0){
-      shuffle.push(Nums.splice(Math.floor(Math.random()*Nums.length),1)[0]+1081000)
+      shuffle.push(Nums.splice(Math.floor(Math.random()*Nums.length), 1)[0]+1081000)
     }
-    return shuffle.slice(0,folders.length+2)
+    return shuffle.slice(0, myFolders.length+2)
   }
-  useEffect(()=>{
-    setImgurls(getNumbers())
-  },[folderss])
 
-  // useEffect (()=>{
-  //   axios({
-  //     url:`http://localhost:8080/api/v1/quiz/{user_id}`,
-  //     method:"GET",
-  //     // headers: setToken()
-  //   })
-  //   .then(res=>{
-  //     setFolders(res.data.뭔가)
-  //   })
-  //   .catch(
-  //     setFolders([])
-  //   )
-  // },[])
-  const Imgurl1 = `https://cdn-icons-png.flaticon.com/512/1081/${Imgurls[(Imgurls.length)-1]}.png`
-  const Imgurl2 = `https://cdn-icons-png.flaticon.com/512/1081/${Imgurls[Imgurls.length-2]}.png`
+
+
+
+  useEffect(() => {
+    setImgUrls(getNumbers())
+  }, [folderss])
+
+
+  const Imgurl1 = `https://cdn-icons-png.flaticon.com/512/1081/${imgUrls[(imgUrls.length)-1]}.png`
+  const Imgurl2 = `https://cdn-icons-png.flaticon.com/512/1081/${imgUrls[imgUrls.length-2]}.png`
+
+
   return (
     <div className="every-folder">
       <Heading className='title'>폴더들</Heading>
-      {/* <br/> */}
+
       <div className='all-cart-mine'>
         <div data-aos="fade-down" data-aos-duration="1500" className='mine'>
           <Link to="#"><img className='folder-icon' src={Imgurl1} alt='그림'/></Link>                    
@@ -63,24 +87,31 @@ const Folder = () => {
           <Heading className='all-cart-mine-title'>내가 만든문제</Heading>
         </div>
       </div>
-      {/* <hr/> */}
+
+
+
       <div className='folders'>
         <Row className='row'>
-          {folders.map((folder,index)=>{
-            const url = `/quiz/folder/${folder.id}`;
-            const Imgurl = `https://cdn-icons-png.flaticon.com/512/1081/${Imgurls[index]}.png`
+          {myFolders.map(({folderId, folderName}, idx) => {
+            const url = `/quiz/folder/${folderId}`;
+            const imgUrl = `https://cdn-icons-png.flaticon.com/512/1081/${imgUrls[idx]}.png`
             return (
-              <Col className='col' sm = {3} key={folder.id}>
+              <Col className='col' sm = {3} key={folderId}>
                 <div data-aos="fade-up"
-                      data-aos-easing="linear"
-                      data-aos-duration="1500" key={url}>
-                  <Link to={url}><img className='folder-icon' src={Imgurl} alt='그림'/></Link>
-                  <p className='title'>{folder.title}</p>
+                  data-aos-easing="linear"
+                  data-aos-duration="1500" key={url}
+                >
+                  <Link to={url}><img className='folder-icon' src={imgUrl} alt='그림'/></Link>
+                  <p className='title'>{folderName}</p>
                 </div>
               </Col>
-            )})} 
+            )
+          })} 
         </Row>
       </div>
+
+
+
       <div>
         <AddQuizPopover folders={folderss} setFolders={setFolderss}  />
       </div>
