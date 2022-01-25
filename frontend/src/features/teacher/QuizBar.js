a225cd726e16bc5c97d10e2d8380a7510515147c
임현홍 <bbnerino@gmail.com>
2022-01-25 18:56:02 +0900

    fix route

--- frontend/src/features/teacher/QuizBar.js
+++ frontend/src/features/teacher/QuizBar.js
@@ -1,36 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Box } from '@chakra-ui/react';
-import axios from 'axios';
-const QuizBar = ({id}) => {
-  const [quiz,setQuiz] =useState([{quizId:3,quizTitle:""},{quizId:4,quizTitle:"어어어"}])
-  useEffect(()=>{
-    const userId = localStorage.getItem("userId")
-    axios({
-      url:`http://localhost:8080/api/v1/quiz/select/favor/${userId}`,
-      method:"GET",
-    })
-    .then(res=>{
-      setQuiz(res.data)
-    })
-    .catch(res=>{
-      setQuiz({quizId:0,quizTitle:"즐겨찾기 퀴즈가 없습니다."})
-    })
-  },[])
+import { Box, Text } from '@chakra-ui/react';
+
+const QuizBar = ({quiz}) => {
   return (
   <Box className='quizbox'>
     <div className='empty'/>
     <Box className='title'>⭐즐겨찾기</Box>
-    <ul className='quizlist'>
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-      {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
-    </ul>
+    {quiz.length?
+      <ul className='quizlist'>
+        {quiz.map(Aquiz=><p key={Aquiz.quizId}>{Aquiz.quizTitle}</p>)}
+      </ul>: 
+      <Text>즐겨찾기한 퀴즈가 없습니다.</Text>}  
   </Box>)
-
 };
 
 export default QuizBar;
