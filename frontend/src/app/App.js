16b3fa2249b12b62430015b6d81add033d27ed98
임현홍 <bbnerino@gmail.com>
2022-02-05 03:17:36 +0900

    fix -conflict

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -6,7 +6,7 @@ import './App.scss'
 import TeacherPage from './TeacherPage';
 import { useEffect, useState } from 'react';
 import MainPage from '../features/mainpage/MainPage';
-import ClassStudent from '../features/ClassStudent/ClassStudent';
+import StudentRoom from '../features/ClassStudent/StudentRoom';
 import Classroom from '../features/ClassTeacher/Classroom'
 import StudentLogin from '../features/ClassStudent/StudentLogin';
 
