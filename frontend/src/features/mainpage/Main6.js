760ebed18523063bc4c0d51779e27a339c205380
이혜진 <hy2jinl22@gmail.com>
2022-02-09 14:01:51 +0900

    fix - mainPage 캐릭터 효과

--- frontend/src/features/mainpage/Main6.js
+++ frontend/src/features/mainpage/Main6.js
@@ -2,47 +2,7 @@ import './scss/Main4.scss'
 
 const Main6 = () => {
   return (
-    <div classNameName='main6'>
-
-
-
-      {/* <div className="slider-wrap">
-        <div id="card-slider" className="slider">
-          <div className="slider-item">
-            <div className="animation-card_image">
-              <img src="https://uznayvse.ru/images/stories2016/uzn_1460039478.jpg" alt="" />
-            </div>
-            <div className="animation-card_content">
-              <h4 className="animation-card_content_title title-2">Charlize Theron 1</h4>
-              <p className="animation-card_content_description p-2">With no contractual commitments comes the freedom of having top notch content created whenever.</p>
-              <p className="animation-card_content_city">New York, NY</p>
-            </div>
-          </div>
-          <div className="slider-item">
-            <div className="animation-card_image">
-              <img src="https://uznayvse.ru/images/stories2016/uzn_1460039478.jpg" alt="" />
-            </div>
-            <div className="animation-card_content">
-              <h4 className="animation-card_content_title title-2">Charlize Theron 2</h4>
-              <p className="animation-card_content_description p-2">With no contractual commitments comes the freedom of having top notch content created whenever.</p>
-              <p className="animation-card_content_city">New York, NY</p>
-            </div>
-          </div>
-          <div className="slider-item">
-            <div className="animation-card_image">
-              <img src="https://uznayvse.ru/images/stories2016/uzn_1460039478.jpg" alt="" />
-            </div>
-            <div className="animation-card_content">
-              <h4 className="animation-card_content_title title-2">Charlize Theron 3</h4>
-              <p className="animation-card_content_description p-2">With no contractual commitments comes the freedom of having top notch content created whenever.</p>
-              <p className="animation-card_content_city">New York, NY</p>
-            </div>
-          </div>
-        </div>
-      </div> */}
-
-
-
+    <div className='main6'>
     </div>
   );
 };
